3b1703cc0994cf4f9456b9edb00961f4eb551f00
Igor Mladenovic <mligor@users.noreply.github.com>
2016-09-10 01:04:44 +0200

    Removed Visual Studio and XCode files. From now only VS Code is supported

--- libs/curl/unpack_curl_src.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set CURL_VERSION=7.44.0
-
-if not exist "%~dp0\curl-src\README" (
-"%~dp0..\..\tools\7za.exe" x curl-%CURL_VERSION%.zip -y -o"%~dp0\_curlsrc"
-move "%~dp0\_curlsrc\curl-%CURL_VERSION%" "%~dp0\curl-src"
-rmdir "%~dp0\_curlsrc"
-)
